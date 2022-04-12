8699fbb8b7bfc70043882f6b64b5fc5bec3d2141
YuYang94724 <the38245691@gmail.com>
2022-04-12 21:10:34 +0800

    06

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("fdkhgfdkjhf");
+		resp.getWriter().append("fdkjflkjflksjl");
+	}
+}
