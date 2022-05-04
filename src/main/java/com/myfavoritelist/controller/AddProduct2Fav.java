72d14346f91967f9680b50148256b8f594279c6d
openthedidi <openthedidi2004@gmail.com>
2022-05-04 17:39:34 +0800

    new wishlist.jsp improve alert AddToShoppingCart

--- src/main/java/com/myfavoritelist/controller/AddProduct2Fav.java
+++ src/main/java/com/myfavoritelist/controller/AddProduct2Fav.java
@@ -1,6 +1,8 @@
 package com.myfavoritelist.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,30 +27,38 @@ public class AddProduct2Fav extends HttpServlet {
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		MemVO memVO = (MemVO) (session.getAttribute("memVO"));
+		PrintWriter out = response.getWriter();
 
 		if (memVO == null) {
-			response.sendRedirect("/CGA101G1/frontend/memLogin/login.html");
+			/**** 沒有會員身分回傳0 ****/
+			out.print(0);
+//			response.sendRedirect("/CGA101G1/frontend/memLogin/login.html");
 		} else {
 
 			Integer memNo = memVO.getMemNo();
 //		Integer memNo = 11003;
 			// 注意要會員編號改成活的
 
-			System.out.println(request.getParameter("ProductNo"));
+//			System.out.println(request.getParameter("ProductNo"));
 			Integer productNo = ((request.getParameter("ProductNo").equals("") ? 0
 					: Integer.valueOf(request.getParameter("ProductNo"))));
-			System.out.println("轉成數字版：" + productNo);
+//			System.out.println("轉成數字版：" + productNo);
 			// 先查有沒有重複
 
 			MyfavoritelistService myfavoritelistService = new MyfavoritelistService();
 			MyfavoritelistVo myfavoritelistVo = myfavoritelistService.getOneByOneMem(memNo, productNo);
 			if (myfavoritelistVo.getProductNo() == 0) {
-				System.out.println("沒有加過要加入清單");
+//				System.out.println("沒有加過要加入清單");
 				myfavoritelistService.addOne(memNo, productNo);
-				response.sendRedirect("/CGA101G1/frontend/Product/HomePageinshop.html");
+//				response.sendRedirect("/CGA101G1/frontend/Product/HomePageinshop.html");
+				/**** 成功加入回傳1 ****/
+				out.print(1);
 			} else {
-				System.out.println("重複添加");
-				response.sendRedirect("/CGA101G1/frontend/Product/HomePageinshop.html");
+
+				/**** 成功加入回傳2 ****/
+//				System.out.println("重複添加");
+				out.print(2);
+//				response.sendRedirect("/CGA101G1/frontend/Product/HomePageinshop.html");
 			}
 		}
 	}
