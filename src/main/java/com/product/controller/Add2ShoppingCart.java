0c701c81a1fc9b283dc557898624bd81240731ce
openthedidi <openthedidi2004@gmail.com>
2022-05-06 12:52:00 +0800

    update url at product.html

--- src/main/java/com/product/controller/Add2ShoppingCart.java
+++ src/main/java/com/product/controller/Add2ShoppingCart.java
@@ -35,19 +35,13 @@ public class Add2ShoppingCart extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		HttpSession session = request.getSession();
 
-		/************** LoginCheck ************************/
-//		if (session.getAttribute("member").getMemNo() == null) {
-//			response.sendRedirect("登入畫面");
-//		} else {
+
 	    /*****************有會員身分開始加入購物車****************/
-//			String memNo = session.getAttribute("member").getMemNo();
-		Integer memNo = 11001;
+
 		List<Cartdetail> orderList = ((List<Cartdetail>) session.getAttribute("shoppingCart")==null?new ArrayList<Cartdetail>():(List<Cartdetail>) session.getAttribute("shoppingCart"));
 		if (session.getAttribute("shoppingCart") == null) {
-//			System.out.println("session is null");
 			Cartdetail orderdetail = new Cartdetail();
-//			System.out.println(request.getParameter("ProductNo"));
-//			System.out.println(request.getParameter("ProductTotalPrice"));
+
 
 			orderdetail.setProductName(request.getParameter("ProductName"));
 			orderdetail.setProductNo(request.getParameter("ProductNo"));
@@ -55,49 +49,33 @@ public class Add2ShoppingCart extends HttpServlet {
 					: Integer.parseInt(request.getParameter("ProductSales")));
 			orderdetail.setProductTotalPrice(Integer.parseInt(request.getParameter("ProductTotalPrice")));
 			
-//			System.out.println(orderdetail.getProductName());
-//			System.out.println(orderdetail.getProductNo());
-//			System.out.println(orderdetail.getProductSales());
-//			System.out.println(orderdetail.getProductTotalPrice());
 			orderList.add(orderdetail);
 			session.setAttribute("shoppingCart", orderList);
 			Gson gson = new Gson();
 			String json = gson.toJson(orderList);
-//			System.out.println(json);
+
 			out.print(json);
-//			response.sendRedirect("frontend/product/HomePageinshop.html");
+
 		} else {
-//			System.out.println("有購物車");
-			Cartdetail newOrderdetail =getCartdetail(request);
-//			System.out.println(newOrderdetail.getProductNo());
-//			System.out.println(newOrderdetail.getProductName());
-//
-//			
-//			System.out.println(orderList.contains(newOrderdetail));
-		
+
+			Cartdetail newOrderdetail =getCartdetail(request);		
 			if(orderList.contains(newOrderdetail)) {
-//				System.out.println("添加舊的書在原本的購物車上");
-				Cartdetail innerOrderdetail =orderList.get(orderList.indexOf(newOrderdetail));
-//				System.out.println("增加的數量："+newOrderdetail.getProductSales());
-//				System.out.println("購物車上的數量："+innerOrderdetail.getProductSales());
-				
+				Cartdetail innerOrderdetail =orderList.get(orderList.indexOf(newOrderdetail));				
 				innerOrderdetail.setProductSales(innerOrderdetail.getProductSales()+newOrderdetail.getProductSales());
 				innerOrderdetail.setProductTotalPrice(innerOrderdetail.getProductTotalPrice()+newOrderdetail.getProductTotalPrice());
 				
-			}else {
-//				System.out.println("添加新的書在原本的購物車上");
-				
+			}else {				
 				orderList.add(newOrderdetail);
 			}
 			
 			Gson gson = new Gson();
 			String json = gson.toJson(session.getAttribute("shoppingCart"));
-//			System.out.println(json);
+
 			out.print(json);
-//			response.sendRedirect("frontend/product/HomePageinshop.html");
+
 		}
 
-//		}
+
 
 	}
 	public Cartdetail getCartdetail(HttpServletRequest request) {
